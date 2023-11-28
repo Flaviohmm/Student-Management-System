a38864a43f9e04bad0297a36eaabad204d8192c8
Flavio Macedo <fhenrique609@gmail.com>
2023-11-27 23:32:53 -0300

    Edit a Student

--- students/views.py
+++ students/views.py
@@ -5,15 +5,17 @@ from django.urls import reverse
 from .models import Student
 from .forms import StudentForm
 
+
 # Create your views here.
 def index(request):
     return render(request, 'students/index.html', {
         'students': Student.objects.all()
     })
     
+    
 def view_student(request, id):
-    student = Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
+
 
 def add(request):
     if request.method == 'POST':
@@ -46,4 +48,22 @@ def add(request):
     return render(request, 'students/add.html', {
         'form': StudentForm()
     })
+    
+
+def edit(request, id):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=id)
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return render(request, 'students/edit.html', {
+                'form': form,
+                'success': True
+            })
+    else:
+        student = Student.objects.get(pk=id)
+        form = StudentForm(instance=student)
         
+    return render(request, 'students/edit.html', {
+        'form': form
+    })      
